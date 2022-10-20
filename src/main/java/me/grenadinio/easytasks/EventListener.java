1071d91eeaf3035aec46727009cac2d2c8c41c4d
BuildTools <unconfigured@null.spigotmc.org>
2022-10-20 23:44:19 +0300

    Task 5 - Пусть у зомби появится имя золотого цвета над головой.

--- src/main/java/me/grenadinio/easytasks/EventListener.java
+++ src/main/java/me/grenadinio/easytasks/EventListener.java
@@ -50,6 +50,9 @@ public class EventListener implements Listener {
             equipment.setChestplate(chestplate);
             equipment.setLeggings(pants);
             equipment.setBoots(boots);
+
+
+            z.setCustomName("§eZombie");
         }
     }
 }
