3e4bdd01351d1ea0100f0ad4d165915f87bc0fef
BuildTools <unconfigured@null.spigotmc.org>
2022-10-21 01:41:21 +0300

    Task 7 - Более адекватная версия

--- src/main/java/me/grenadinio/easytasks/EventListener.java
+++ src/main/java/me/grenadinio/easytasks/EventListener.java
@@ -13,7 +13,6 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
-import org.bukkit.scheduler.BukkitScheduler;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -33,52 +32,48 @@ public class EventListener implements Listener {
         if(event.getBlock().getType() == Material.DIRT || event.getBlock().getType() == Material.GRASS) {
             AtomicInteger seconds = new AtomicInteger(5);
 
-//            Timer t = new Timer();
-//            TimerTask tt = new TimerTask() {
-//                @Override
-//                public void run() {
-//                    if(seconds.get() == 0){
-//                        t.cancel();
-//                        return;
-//                    }
-//                    event.getPlayer().sendMessage("Seconds: " + seconds.getAndDecrement());
-//                };
-//            };
-//            t.scheduleAtFixedRate(tt, 0L, TimeUnit.SECONDS.toMillis(1));
+            Timer t = new Timer();
+            TimerTask tt = new TimerTask() {
+                @Override
+                public void run() {
+                    if(seconds.get() == 0){
+                        t.cancel();
+                        return;
+                    }
+                    event.getPlayer().sendMessage("Seconds: " + seconds.getAndDecrement());
+                };
+            };
+            t.scheduleAtFixedRate(tt, 0L, TimeUnit.SECONDS.toMillis(1));
 
 
-            BukkitScheduler scheduler = Bukkit.getScheduler();
-            scheduler.scheduleSyncRepeatingTask((Main.getPlugin()), () -> {
 
-                if(seconds.get() == 0) {
-                    Location l = event.getBlock().getLocation();
-                    Zombie z = (Zombie) event.getBlock().getWorld().spawnEntity(l.add(0, 1, 0), EntityType.ZOMBIE);
-                    EntityEquipment equipment = z.getEquipment();
+            Bukkit.getScheduler().scheduleSyncDelayedTask((Main.getPlugin()), () -> {
 
-                    ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
-                    ItemStack pants = new ItemStack(Material.LEATHER_LEGGINGS);
-                    ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
-                    ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
+                Location l = event.getBlock().getLocation();
+                Zombie z = (Zombie) event.getBlock().getWorld().spawnEntity(l.add(0, 1, 0), EntityType.ZOMBIE);
+                EntityEquipment equipment = z.getEquipment();
 
-                    LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) helmet.getItemMeta();
+                ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+                ItemStack pants = new ItemStack(Material.LEATHER_LEGGINGS);
+                ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+                ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
 
-                    leatherArmorMeta.setColor(Color.YELLOW);
+                LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) helmet.getItemMeta();
 
-                    helmet.setItemMeta(leatherArmorMeta);
-                    chestplate.setItemMeta(leatherArmorMeta);
-                    pants.setItemMeta(leatherArmorMeta);
-                    boots.setItemMeta(leatherArmorMeta);
+                leatherArmorMeta.setColor(Color.YELLOW);
 
-                    equipment.setHelmet(helmet);
-                    equipment.setChestplate(chestplate);
-                    equipment.setLeggings(pants);
-                    equipment.setBoots(boots);
+                helmet.setItemMeta(leatherArmorMeta);
+                chestplate.setItemMeta(leatherArmorMeta);
+                pants.setItemMeta(leatherArmorMeta);
+                boots.setItemMeta(leatherArmorMeta);
 
-                    z.setCustomName(ChatColor.YELLOW + "Zombie");
-                    scheduler.cancelTasks((Main.getPlugin()));
-                }
-                event.getPlayer().sendMessage("Seconds: " + seconds.getAndDecrement());
-            }, 0,20);
+                equipment.setHelmet(helmet);
+                equipment.setChestplate(chestplate);
+                equipment.setLeggings(pants);
+                equipment.setBoots(boots);
+
+                z.setCustomName(ChatColor.YELLOW + "Zombie");
+            }, 100);
         }
     }
 }
