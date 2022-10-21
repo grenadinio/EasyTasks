4ee1118919c56aaa552bd88dc0cafcf1c4376f2c
BuildTools <unconfigured@null.spigotmc.org>
2022-10-21 16:29:42 +0300

    Task 10 - Пусть при каждом ударе по одному конкретному (но не каждому) зомби игроку пишется в чат, сколько урона он нанес и сколько здоровья осталось у зомби после получения урона.

--- src/main/java/me/grenadinio/easytasks/EventListener.java
+++ src/main/java/me/grenadinio/easytasks/EventListener.java
@@ -2,39 +2,47 @@ package me.grenadinio.easytasks;
 
 import org.bukkit.*;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 
+import java.text.DecimalFormat;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class EventListener implements Listener {
+    private static final NamespacedKey zombie_1_key = new NamespacedKey(Main.getPlugin(), "zombie_1_key");
+    private static final DecimalFormat damage_format = new DecimalFormat("#.##");
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
+    public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage("Привет");
-        event.setJoinMessage("Privet, " + event.getPlayer().getName() +" or Привет");
+        event.setJoinMessage("Privet, " + event.getPlayer().getName() + " or Привет");
     }
 
     @EventHandler
-    public void onBlockBreak(BlockBreakEvent event){
-        if(event.getBlock().getType() == Material.DIRT || event.getBlock().getType() == Material.GRASS) {
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (event.getBlock().getType() == Material.DIRT || event.getBlock().getType() == Material.GRASS) {
             AtomicInteger seconds = new AtomicInteger(5);
 
             Timer t = new Timer();
             TimerTask tt = new TimerTask() {
                 @Override
                 public void run() {
-                    if(seconds.get() == 0){
+                    if (seconds.get() == 0) {
                         t.cancel();
                         return;
                     }
@@ -49,8 +57,8 @@ public class EventListener implements Listener {
                 Vector direction = event.getPlayer().getLocation().getDirection();
                 Location block_loc = player_loc.add(direction.multiply(2));
 
-                Location loc_zombie_left = block_loc.clone().add(-1,0,0);
-                Location loc_zombie_right = block_loc.clone().add(1,0,0);
+                Location loc_zombie_left = block_loc.clone().add(-1, 0, 0);
+                Location loc_zombie_right = block_loc.clone().add(1, 0, 0);
 
 
                 //Leather armour
@@ -61,50 +69,68 @@ public class EventListener implements Listener {
 
                 //Yellow colored leather armour
                 LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) helmet.getItemMeta();
-                leatherArmorMeta.setColor(Color.YELLOW);
+                if (leatherArmorMeta != null) {
+                    leatherArmorMeta.setColor(Color.YELLOW);
+                }
                 helmet.setItemMeta(leatherArmorMeta);
                 chestplate.setItemMeta(leatherArmorMeta);
                 pants.setItemMeta(leatherArmorMeta);
                 boots.setItemMeta(leatherArmorMeta);
 
                 //Check location for left zombie
-                if(loc_zombie_left.clone().add(0,-1,0).getBlock().getType() != Material.AIR
+                if (loc_zombie_left.clone().add(0, -1, 0).getBlock().getType() != Material.AIR
                         && loc_zombie_left.getBlock().getType() == Material.AIR
-                        && loc_zombie_left.clone().add(0,1,0).getBlock().getType() == Material.AIR) {
+                        && loc_zombie_left.clone().add(0, 1, 0).getBlock().getType() == Material.AIR) {
 
                     Zombie z = (Zombie) event.getPlayer().getWorld().spawnEntity(loc_zombie_left, EntityType.ZOMBIE);
                     EntityEquipment equipment = z.getEquipment();
 
-                    equipment.setHelmet(helmet);
-                    equipment.setChestplate(chestplate);
-                    equipment.setLeggings(pants);
-                    equipment.setBoots(boots);
+                    if (equipment != null) {
+                        equipment.setHelmet(helmet);
+                        equipment.setChestplate(chestplate);
+                        equipment.setLeggings(pants);
+                        equipment.setBoots(boots);
+                    }
 
                     z.setCustomName(ChatColor.YELLOW + "Zombie");
-                }
-                else{
+
+                    z.getPersistentDataContainer().set(zombie_1_key, PersistentDataType.STRING, "mark");
+                } else {
                     event.getPlayer().sendMessage("Слева не получилось");
                 }
 
                 //Check location for right zombie
-                if(loc_zombie_right.clone().add(0,-1,0).getBlock().getType() != Material.AIR
+                if (loc_zombie_right.clone().add(0, -1, 0).getBlock().getType() != Material.AIR
                         && loc_zombie_right.getBlock().getType() == Material.AIR
-                        && loc_zombie_right.clone().add(0,1,0).getBlock().getType() == Material.AIR) {
+                        && loc_zombie_right.clone().add(0, 1, 0).getBlock().getType() == Material.AIR) {
 
                     Zombie z = (Zombie) event.getPlayer().getWorld().spawnEntity(loc_zombie_right, EntityType.ZOMBIE);
                     EntityEquipment equipment = z.getEquipment();
 
-                    equipment.setHelmet(helmet);
-                    equipment.setChestplate(chestplate);
-                    equipment.setLeggings(pants);
-                    equipment.setBoots(boots);
-
+                    if (equipment != null) {
+                        equipment.setHelmet(helmet);
+                        equipment.setChestplate(chestplate);
+                        equipment.setLeggings(pants);
+                        equipment.setBoots(boots);
+                    }
                     z.setCustomName(ChatColor.YELLOW + "Zombie");
-                }
-                else{
+                } else {
                     event.getPlayer().sendMessage("Справа не получилось");
                 }
             }, 100);
+        }
+    }
+
+    @EventHandler
+    public void onHurt(EntityDamageByEntityEvent event) {
+        if (!(event.getDamager() instanceof Player)) return;
+        if (!(event.getEntity() instanceof LivingEntity)) return;
+        if (Objects.equals(event.getEntity().getPersistentDataContainer().get(zombie_1_key, PersistentDataType.STRING), "mark")) {
+            double damage = event.getFinalDamage();
+            String rounded_damage = damage_format.format(damage);
+            LivingEntity entity = (LivingEntity) event.getEntity();
+            String entiry_health = damage_format.format(entity.getHealth() - damage);
+            event.getDamager().sendMessage("Нанесено " + rounded_damage + " урона. У моба осталось " + entiry_health + " хп.");
         }
     }
 }
